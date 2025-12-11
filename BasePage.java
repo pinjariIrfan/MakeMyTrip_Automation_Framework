58f8edab884c5ce5c14c18b5431de9d0700c7f5a
Pinjari Irfan <irfan879069@gmail.com>
2025-12-11 16:55:51 +0530

    Create BasePage class for Selenium page objects

---
+++ BasePage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class BasePage {
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+    }
+    
+    // Common methods can be added here
+}
