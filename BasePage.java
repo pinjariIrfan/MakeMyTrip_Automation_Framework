6e92f01145e29dddc570eaf2d7691332b5c8856d
Pinjari Irfan <irfan879069@gmail.com>
2025-12-11 22:35:03 +0530

    Update BasePage.java

--- BasePage.java
+++ BasePage.java
@@ -3,17 +3,23 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.WebElement;
 import java.time.Duration;
 
 public class BasePage {
+
     protected WebDriver driver;
     protected WebDriverWait wait;
-    
+
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
-    
-    // Common methods can be added here
+
+    // Simple common wait method
+    public void waitForElement(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
 }
